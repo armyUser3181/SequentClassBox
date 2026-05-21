0d886b44195f4a8bb1cc0ad65bb52b0e7d4acc0a
armyUser3181 <yong3181.25.1@gmail.com>
2026-05-21 23:35:15 +0900

    개선하고 있는 중

--- EventElementClass.js
+++ EventElementClass.js
@@ -1,12 +1,24 @@
 
 import EventActionClass from "./EventActionClass.js";
 
+/**
+ * @typedef {(args : import("./EventActionClass").CallerArgs) => void} CallerElement
+ */
+/**
+ * @typedef {CallerElement[]} Callers
+ */
+
 export default class EventElementClass {
 
     /**
      * @type {EventActionClass[]}
      */
-    eventActions = [];
+    actions = [];
+
+    /**
+     * @type {Callers}
+     */
+    callers = [];
 
     /**
      * @type {string}
@@ -17,26 +29,45 @@ export default class EventElementClass {
     }
 
     push(eventAction) {
-        this.eventActions.push(eventAction);
+        this.actions.push(eventAction);
         return this;
     }
 
     clear() {
-        this.eventActions.forEach(action => action.unbind);
-        this.eventActions = [];
+        this.actions.forEach(action => action.unbind);
+        this.actions = [];
         return this;
     }
 
     get setup() {
         return {
             classic : ()=>{
-                
+                this.callers[0] = ({
+                    callback
+                }) => {
+                    callback();
+                }
+                this.actions.forEach((action) => {
+                    action.caller = this.callers[0];
+                });
             },
             chain : ()=>{
-
+                const number = 0;
+                this.callers[0] = ({
+                    callback
+                }) => {
+                    callback();
+                    this.actions[number].unbind;
+                    number++;
+                    this.actions[number].bind;
+                }
+                this.actions.forEach((action) => {
+                    action.caller = this.callers[0];
+                });
+                this.actions[0].bind
             },
             cond : ()=>{
-
+                
             },
             call : ()=>{
 
